2f45d802ae6a7f8be500c80b364dd265a425cab3
Tarun <tarunmohantys@gmail.com>
2020-10-28 16:16:30 +0530

    First unit test and url mappin, dummy view

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+import unittest
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_review_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes 
+        # to check out its homepage
+
+        self.browser.get('http://localhost:8000')
+
+        #she notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
